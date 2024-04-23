dd2495f3468503fe004dae156ed65a43cba0e64d
Masutha Arnold <85495755+arnoxide@users.noreply.github.com>
2024-04-24 04:16:18 +1400

    here we go!!!!!!!!!!!

---
+++ assesment.grad001.arnoldmasutha/src/test/java/com/enviro/assesment/grad001/arnoldmasutha/WasteSortingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assesment.grad001.arnoldmasutha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteSortingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
